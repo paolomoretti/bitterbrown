ea39c6fff26fbe4781a7c2d633e4c69b9c37673c
Paolo Moretti <littlebrown@gmail.com>
2014-08-17 18:46:08 +0100

    Refactor

--- grunt-css-cleaner/index.php
+++ grunt-css-cleaner/index.php
@@ -1,5 +1,5 @@
 <base href="http://bitterbrown.github.io/grunt-css-cleaner/" />
-<?php
-$website = file_get_contents("http://bitterbrown.github.io/grunt-css-cleaner/");
-echo $website;
-?>
+  <?php
+  $website = file_get_contents("http://bitterbrown.github.io/grunt-css-cleaner/");
+  echo $website;
+  ?>
---
+++ prolific/index.php
@@ -0,0 +1,5 @@
+<base href="http://bitterbrown.github.io/prolific/" />
+<?php
+$website = file_get_contents("http://bitterbrown.github.io/prolific/");
+echo $website;
+?>
